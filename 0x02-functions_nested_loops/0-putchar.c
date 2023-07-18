a6b3d50bc3da8ae308941c657aa47702a4578da5
Iamtopisticks <t.pompey24@yahoo.com>
2023-07-18 07:00:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 /**
  * main - print _putchar
- * is written by mbar
- * Return: 0
+ *
+ * Return: (0)
+ *
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; i <= 7, i++)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
+
 return (0);
-}
