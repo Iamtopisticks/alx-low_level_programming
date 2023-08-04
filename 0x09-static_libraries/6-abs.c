be7b61e1c4e7cbce4c2e8a731f8433f5f6488e4a
Iamtopisticks <t.pompey24@yahoo.com>
2023-08-04 07:17:24 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - function to find the absolute value of a number
+ * @r: the value
+ * Description: The standard library provides a similar function
+ *
+ * Return: (0)
+ *
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+
+		return (r);
+	else
+		return (r * -1);
+}
